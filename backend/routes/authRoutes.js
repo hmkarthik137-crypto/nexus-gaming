31e37980f53642605ae576007e19861e1e00b979
hmkarthik137-crypto <hmkarthik137@gmail.com>
2026-06-12 23:24:04 +0530

    Fix import case for Order model in authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/User.js";
-import Order from "../models/Order.js";
+import Order from "../models/order.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 import {
