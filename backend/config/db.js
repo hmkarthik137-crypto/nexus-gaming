0c884ae34828bc40958a25687d52c141d5f121d0
hmkarthik137-crypto <hmkarthik137@gmail.com>
2026-06-12 23:48:05 +0530

    Add console log for MONGO_URI
    Log the MongoDB URI for debugging purposes.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import dns from "dns";
-
+console.log(process.env.MONGO_URI);
 const connectDB = async () => {
   try {
     dns.setServers(["8.8.8.8", "1.1.1.1"]);
